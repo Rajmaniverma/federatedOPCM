a34cdfa25882af7d0a4e5360ac2c24829b616ab6
Rajmaniverma <rajmaniverma8051@gmail.com>
2026-06-07 14:45:23 +0530

    supplier c db.js

--- supplier-c/db.js
+++ supplier-c/db.js
@@ -1,21 +1,15 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+const pool = mysql.createPool({
+  host: process.env.DB_HOST.trim(),
+  user: process.env.DB_USER.trim(),
+  password: process.env.DB_PASSWORD.trim(),
+  database: process.env.DB_NAME.trim(),
+  port: Number(process.env.DB_PORT),
 
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error("Database connection failed:", err);
-    return;
-  }
-
-  console.log("MySQL Connected Successfully");
-});
-
-module.exports = connection;
+module.exports = pool;
