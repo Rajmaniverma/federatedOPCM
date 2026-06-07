cea6a7873b9a29e03ae392d92ecdb5604a1015db
Rajmaniverma <rajmaniverma8051@gmail.com>
2026-06-07 14:47:16 +0530

    recorrect the supplier c db.js

--- supplier-c/db.js
+++ supplier-c/db.js
@@ -1,15 +1,25 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST.trim(),
-  user: process.env.DB_USER.trim(),
-  password: process.env.DB_PASSWORD.trim(),
-  database: process.env.DB_NAME.trim(),
-  port: Number(process.env.DB_PORT),
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+    return;
+  }
+
+  console.log("MySQL Pool Connected Successfully");
+  connection.release();
 });
 
 module.exports = pool;
